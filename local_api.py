809abb72e9fb6ee1c03ad9f62225ef344991b0ab
Joshua <joshuaedrake@gmail.com>
2025-12-01 15:10:44 -0800

    Refactor code structure for improved readability and maintainability

--- local_api.py
+++ local_api.py
@@ -1,14 +1,13 @@
-# import json
-# import requests
+import requests
 
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None  # Your code here
+# Send a GET using the URL http://127.0.0.1:8000
+r = requests.get("http://127.0.0.1:8000/")
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+# Print the status code
+print(f"Status Code: {r.status_code}")
+# Print the welcome message
+print(f"Result: {r.json()}")
 
 data = {
     "age": 37,
@@ -27,10 +26,10 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None  # Your code here
+# Send a POST using the data above
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+# Print the status code
+print(f"Status Code: {r.status_code}")
+# Print the result
+print(f"Result: {r.json()}")
